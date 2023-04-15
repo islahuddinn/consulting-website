1d8aabd673429b9275050fa06ceda5d049d4b3e0
Sallu-sb <m.chsallu87@gmail.com>
2023-04-15 19:49:20 +0500

    master

---
+++ README.md
@@ -0,0 +1,17 @@
+# Welcome to Barakatax and bookkeeping website.
+
+We are the real tax professionals!
+As Enrolled Agents, we admitted practicing in all 50 states through the Department of the Treasury Internal Revenue Service(IRS).
+
+## Used NPM Packages 
+#### Create React App
+#### React Router Dom
+#### React Bootstrap
+#### Slick Slider
+#### Font Awesome
+#### Counter Up
+#### Visibility sensor
+#### React Modal Video
+#### Slick Slider
+#### React Share
+#### React hook form
---
+++ src/components/Banners/BannerOne/BannerOne.js
@@ -0,0 +1,41 @@
+// import { Link } from 'react-router-dom';
+import heroShape from '../../../assets/images/home/hero-shape.png';
+import heroBgImage from '../../../assets/images/home/hero.jpg';
+// import Video from '../../Video/Video';
+
+const BannerOne = () => {
+    return (
+        <section className="hero-banner-01" style={{ backgroundImage: `url(${heroBgImage})` }} >
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-7 col-md-10">
+                        <div className="banner-content">
+                            <div className="sub-heading">
+                                One of the best tax consulting agency
+                        	</div>
+                            <h1>Get Solutions</h1>
+                            <p>
+                            We help our clients to reduce tax liability by 5-35%, slash business expenses, stimulate cash flow, and navigate 
+                            the best financial resource with tax reduction strategies and planning. So they can achieve profitable long-term growth.
+                            </p>
+
+                            {/* <ul>
+                                <li><Link className="quomodo-btn" to="/">Get Started Now <i className="fa fa-long-arrow-right"></i></Link></li>
+                                <li>
+                                    <Video videoLink="LXb3EKWsInQ" title="Intro Video" />
+                                </li>
+                            </ul> */}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <!-- Shape --> */}
+            <div className="hero-shape"><img src={heroShape} alt="" /></div>
+
+        </section>
+    );
+};
+
+export default BannerOne;
