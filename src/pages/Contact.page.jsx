2924698288a177a3f56ffc58e8702a06aaaaf01c
Sallu-sb <m.chsallu87@gmail.com>
2023-04-16 12:15:30 +0500

    error resolved

--- src/pages/Contact.page.jsx
+++ src/pages/Contact.page.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="quomodo-map grayscale"> 
-                             <iframe src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;coord=39.966528,-75.158284&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=p&amp;z=16&amp;iwloc=B&amp;output=embed" scrolling="no"></iframe>                      
+                             <iframe title = "myFrame" src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;coord=39.966528,-75.158284&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=p&amp;z=16&amp;iwloc=B&amp;output=embed" scrolling="no"></iframe>                      
                             
                             </div>
                         </div>
--- src/pages/Portfolio.page.jsx
+++ src/pages/Portfolio.page.jsx
@@ -1,6 +1,6 @@
 import BannerTwo from '../components/Banners/BannerTwo/BannerTwo';
 import PortfolioTwo from '../components/Portfolios/PortfolioTwo/PortfolioTwo';
-import Team from '../components/Team/Team';
+// import Team from '../components/Team/Team';
 import Testimonial from '../components/Testimonial/Testimonial';
 
 const Portfolio = () => {
